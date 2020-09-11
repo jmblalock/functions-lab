039012140bf127411223742f2573f0fbd68abba5
j-blalock <justin.m.blalock@gmail.com>
2020-09-11 16:50:20 -0500

    completed problem 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('functions lab');
+/*
+Problem 1. printGreeting
+Write a function called printGreeting with a parameter name that returns a greeting with the argument interpolated into the greeting.
+
+console.log(printGreeting("Slimer"));
+=> Hello there, Slimer!
+*/
+
+function printGreeting(name) {
+    return ('Hello there, ' + name + '!');
+}
+
+console.log(printGreeting("Slimer"));
