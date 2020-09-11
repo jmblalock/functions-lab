b570c8c596759c0b0ca696762d063234b346e030
j-blalock <justin.m.blalock@gmail.com>
2020-09-11 17:54:51 -0500

    completed problem 3

--- js/app.js
+++ js/app.js
@@ -30,3 +30,42 @@ function reverseWordOrder(s) {
 
 console.log(reverseWordOrder("Ishmael me Call"));
 console.log(reverseWordOrder("I use Lâncome on my comb"));
+
+/*
+Problem 3. calculate
+Write a function called calculate.
+
+This function should take three arguments: two numbers and a string.
+
+Name the parameters num1, num2, and operation.
+
+If if the function is called with the third argument as "add", it should return the sum of num1 and num2.
+
+If if the function is called with the third argument as "sub", it should return return num1 minus num2.
+
+Do the same thing for multiplication "mult", division "div", and exponent "exp" (where num2 is the exponent of num1).
+
+console.log(calculate(4, 3, "sub"));
+
+=> 1
+console.log(calculate(4, 3, "exp"));
+
+=> 64
+*/
+
+function calculate(num1, num2, operation) {
+    if (operation === "add") {
+        return (num1 + num2);
+    } else if (operation === 'sub') {
+        return (num1 - num2);
+    } else if (operation === 'mult') {
+        return (num1 * num2);
+    } else if (operation === 'div') {
+        return (num1 / num2);
+    } else if (operation === 'exp') {
+        return (Math.pow(num1, num2))
+    }
+}
+
+console.log(calculate(4, 3, "sub"));
+console.log(calculate(4, 3, "exp"));
